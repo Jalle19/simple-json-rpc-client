29ab72e441b42dad817def73f6742ba3631ec87d
Sam Stenvall <neggelandia@gmail.com>
2014-07-03 16:38:14 +0300

    don't start the test suite before the built-in web server is available

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -18,6 +18,10 @@ echo sprintf(
 		'JSON-RPC server started on %s:%d with PID %d', WEB_SERVER_HOST, WEB_SERVER_PORT, $pid
 ).PHP_EOL;
 
+// Wait for the built-in web server to start accepting requests
+while (!@fsockopen(WEB_SERVER_HOST, WEB_SERVER_PORT))
+	usleep(1000);
+
 // Kill the web server when the process ends
 register_shutdown_function(function() use ($pid) {
 	echo sprintf('Killing process with ID %d', $pid).PHP_EOL;
